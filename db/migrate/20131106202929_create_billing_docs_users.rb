a130cf3e734be47f4deeb2ca427a07cb454693e9
Adi Singh <adityasingh89@gmail.com>
2013-11-14 17:26:22 -0500

    git removal of biling_docs_user, _create_billing_docs_users.rb, and invoices_controller_spec.rb

--- db/migrate/20131106202929_create_billing_docs_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateBillingDocsUsers < ActiveRecord::Migration
-  def change
-    create_table :billing_docs_users do |t|
-      t.references :billing_doc
-      t.references :user
-    end
-  end
-end
